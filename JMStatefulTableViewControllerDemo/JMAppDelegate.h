710114d4058b3661bd24bb80925c5f9b1371a4d3
Jake Marsh <jake@thejakemarsh.com>
2012-05-03 09:15:52 -0500

    Initial Commit

---
+++ JMStatefulTableViewControllerDemo/JMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JMAppDelegate.h
+//  JMStatefulTableViewControllerDemo
+//
+//  Created by Jake Marsh on 5/3/12.
+//  Copyright (c) 2012 Rubber Duck Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JMStatefulTableViewControllerDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JMStatefulTableViewControllerDemo
+//
+//  Created by Jake Marsh on 5/3/12.
+//  Copyright (c) 2012 Rubber Duck Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JMAppDelegate class]));
+    }
+}
